9871012ec512594619f4618a0b896db6907bca02
aarondavidreal-lab <aarondavid.real@gmail.com>
2026-04-15 17:40:16 +0100

    format date

--- vite-project/src/components/Result.jsx
+++ vite-project/src/components/Result.jsx
@@ -1,17 +1,25 @@
 export default function Result({weatherData, city}) {
 
+
+if (!weatherData || !weatherData.current) return null
+
+const formattedDate = new Date(weatherData.current.time).toLocaleDateString("en-US", {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    }
+    ) 
     return (
-        <div className="current-weather-info">
-
-            {city && <h1>{city.toUpperCase()}</h1>}
-
-             {weatherData && weatherData.current ? 
-             <>
-                <p>{weatherData.current.time}</p>
+    <>
+        {weatherData && weatherData.current ? 
+            <div className="current-weather-info">
+                {city && <h1>{city.toUpperCase()}</h1>}
+                <p>{formattedDate}</p>
                 <p>{weatherData.current.apparent_temperature}°C</p> 
-             </>
-             : null}
-        </div>
+            </div>
+            : <p>Weather data not available yet...</p>}
+    </>
     )
    
 }  
