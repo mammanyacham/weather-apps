c326f44bc335e3949dca160bbbdbe416ab3b35f1
aarondavidreal-lab <aarondavid.real@gmail.com>
2026-05-13 18:40:49 +0100

    style units section

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,9 +9,8 @@ export default function App() {
   const [formData, setFormData] = useState()
   const [searchQuery, setSearchQuery] = useState()
   const [weatherData, setWeatherData] = useState()
+  const [units, setUnits] = useState()
 
-
- 
 
   function getFormData(e) {
     const value = e.target.value
@@ -27,40 +26,39 @@ export default function App() {
   }
   
 
-
   useEffect(() => {
     async function fetchData() {
 
       if (!searchQuery) return
 
 
-    try{
-      const geoResponse = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${searchQuery}&count=10&language=en&format=json`)
+      try{
+        const geoResponse = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${searchQuery}&count=10&language=en&format=json`)
 
-      const geoData = await geoResponse.json()
-      
-      const geoCodeResults = geoData.results[0]
-      if (!geoCodeResults || geoCodeResults.length === 0){
-        return
+        const geoData = await geoResponse.json()
+        
+        const geoCodeResults = geoData.results[0]
+        if (!geoCodeResults || geoCodeResults.length === 0){
+          return
+        }
+
+        
+        const latitude = geoCodeResults.latitude
+        const longitude = geoCodeResults.longitude
+
+
+        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m,weather_code`)
+
+          const weatherData = await weatherResponse.json()
+
+
+          setWeatherData(weatherData)
+          console.log("Current weather:", weatherData)
+      } catch(error) {
+          console.error("Error fetching geocoding data:", error)
+
       }
-
-       
-       const latitude = geoCodeResults.latitude
-       const longitude = geoCodeResults.longitude
-
-
-       const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m,weather_code`)
-
-        const weatherData = await weatherResponse.json()
-
-
-        setWeatherData(weatherData)
-        console.log("Current weather:", weatherData)
-    } catch(error) {
-        console.error("Error fetching geocoding data:", error)
-
-    }
-    
+      
     }
 
      fetchData()    
--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -1,16 +1,42 @@
 import appLogo from '/images/logo.svg'
+import dropdownIcon from '/images/icon-dropdown.svg'
+import unitsIcon from '/images/icon-units.svg'
 
 export default function Header() {
- return(
- <header>
-    <img src={appLogo}/>
+
     
-    <select className='dropdown' type='dropdown'>
-        <option>units</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-    </select>
- </header>)
+ return(
+<>
+    <header>
+        <img src={appLogo} alt='logo' className='app-logo'/>
+        <div className='dropdown-btn-div'>
+            <button className='dropdown-btn'>
+                <img src={unitsIcon} alt="Units" />
+                    units
+                <img src={dropdownIcon} alt="Dropdown" />
+            </button>
+        </div>
+    </header>
+    <div className='dropdown'>
+        <button><p>Switch to Imperial</p></button>
+        <div>
+            <span className='dropdown-section'>Temperature</span>
+            <p className='tool'>Celsius (C)</p>
+            <p className='tool'>Fahrenheit (F)</p>
+        </div>
+
+        <div>
+            <span className='dropdown-section'>Wind Speed</span>
+            <p className='tool'><span>km/h</span></p>
+            <p className='tool'><span>mph</span></p>
+        </div>
+
+        <div>
+            <span className='dropdown-section'>Percipitation</span>
+            <p className='tool'><span>Millimeters (mm)</span></p>
+            <p className='tool'><span>inches (in)</span></p>
+        </div>
+    </div> 
+</>
+)
 }
